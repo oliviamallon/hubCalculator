6e804753510ee1eefe0562da455588fee4a8e55e
Olivia Mallon <77295000+oliviamallon@users.noreply.github.com>
2021-11-01 08:47:39 +0000

    updating files

--- HubMeal.js
+++ HubMeal.js
@@ -3,10 +3,19 @@ var namePeoples = [];
 var usersCosts = [];
 var mealCosts = [];
 var usersMeals = [];
+var numWeeks;
 
 function start() {
+    document.getElementById('numPeople').value = "";
+    document.getElementById('subNumberPeople').style.display = "inline-block";
+    document.getElementById('names').style.display = "none";
     document.getElementById('tblCosts').style.display = "none";
+    document.getElementById('tblCosts').innerHTML =""; 
     document.getElementById('tblOutput').style.display = "none";
+    document.getElementById("names").innerHTML = "";
+    document.getElementById('noWeeks').innerHTML="";
+    document.getElementById('noWeeks').style.display="none";
+    numPeople = 0;
     namePeoples = [];
     usersCosts = [];
     mealCosts = [];
@@ -14,7 +23,6 @@ function start() {
 }
 
 function noPeople() {
-
     numPeople = document.getElementById("numPeople").value;
 
     if (numPeople == null || numPeople == "") {
@@ -24,53 +32,117 @@ function noPeople() {
     } else {
         peopleNames(numPeople);
         document.getElementById('subNumberPeople').style.display = "none";
+        document.getElementById('names').style.display = "block";
     }
 }
 
 function peopleNames(people) {
+    document.getElementById("names").innerHTML += '<b>Please enter the names of people in your household: <b> <br>';
     for (i = 0; i < people; i++) {
         var sub = 'person' + (i + 1);
         document.getElementById("names").innerHTML += '<p>Person ' + (i + 1) + ': <input type="text" id="' + sub + '"></p>';
     }
     if (people != 0) {
-        document.getElementById("names").innerHTML += '<button id="submitNames" class="btn" onclick="returnNames();">Submit Name</button><br><br>';
+        document.getElementById("names").innerHTML += '<button id="submitNames" class="btn" onclick="weeksCalc();">Submit Names</button><br><br>';
     }
+}
+
+function weeksCalc() {
+    document.getElementById('submitNames').style.display = "none";
+    document.getElementById('noWeeks').style.display = "block";
+    var weeksDiv = document.getElementById('noWeeks');
+    var text = '<form action="#"><p><b>Please enter the number of weeks you would like to calculate: </b><input type="number" id="numWeeks" name="numWeeks"  maxlength="2"></p></form><button class="btn" id="subNumberWeeks" onclick="returnNames();">Submit</button>';
+    weeksDiv.innerHTML = text;
 }
 
 function returnNames() {
-    document.getElementById("submitNames").style.display = "none";
-    for (i = 0; i < numPeople; i++) {
-        var title = 'person' + (i + 1);
-        var data = document.getElementById(title).value;
-        namePeoples.push(data);
-    }
+    numWeeks = document.getElementById("numWeeks").value;
 
-    document.getElementById('here').innerHTML += '<br>';
+    if (isNaN(numWeeks)) {
+        alert("Please enter in a number! ");
+    } else if (numWeeks < 0) {
+        alert("Please enter in a value of 1 or higher");
+    } else if (numWeeks > 30) {
+        alert("Please enter a number less than 30");
+    } else {
+        document.getElementById("subNumberWeeks").style.display = "none";
+        //creates basic table - no names incl.
+        createTable();
+        for (i = 0; i < numPeople; i++) {
+            //adds peoples names to an Array to be used later on for table creation
+            var title = 'person' + (i + 1);
+            var data = document.getElementById(title).value;
+            namePeoples.push(data);
+        }
 
-    for (y = (numPeople - 1); y > -1; y--) {
-        createCol(namePeoples[y], y);
+        for (y = (numPeople - 1); y > -1; y--) {
+            //creates columns for people????
+            createCol(namePeoples[y], y);
+        }
+        document.getElementById('tblCosts').style.display = "block";
+
     }
-    document.getElementById('tblCosts').style.display = "block";
 
 }
 
+function createTable() {
+
+    var tableToAdd = "";
+    document.getElementById('tblCosts').innerHTML = "";
+
+    for (i = 0; i < numWeeks; i++) {
+        var weekNo = i + 1;
+        tableToAdd += '<h2>Week ' + weekNo + '</h2><table id="costs' + weekNo + '">';
+        tableToAdd += '<tr id="w' + weekNo + 'titles"><th>Day of Week</th><th>Cost</th><th>No. People</th><th>Select all as present</th></tr>';
+        tableToAdd += '<tr id="w' + weekNo + 'mon"><td>Monday</td><td><input type="currency" placeholder="0.00" size="5" min="0.00" max="10000.00" maxlength="6"step="0.01" name="w' + weekNo + 'cost" id="w' + weekNo + 'cost1"></td><td><p id="w' + weekNo + 'people1" class="w' + weekNo + 'people">0</p></td><td><input type="checkbox" id="w' + weekNo + 'all1" onclick="selectAll('+weekNo+' , 1)">Select All</input></td></tr>';
+        tableToAdd += '<tr id="w' + weekNo + 'tues"><td>Tuesday</td><td><input type="currency" placeholder="0.00" size="5" min="0.00" max="10000.00" maxlength="6"step="0.01" name="w' + weekNo + 'cost" id="w' + weekNo + 'cost2"></td><td><p id="w' + weekNo + 'people2" class="w' + weekNo + 'people">0</p></td><td><input type="checkbox" id="w' + weekNo + 'all2" onclick="selectAll('+weekNo+' , 2)">Select All</input></td></tr>';
+        tableToAdd += '<tr id="w' + weekNo + 'wed"><td>Wednesday</td><td><input type="currency" placeholder="0.00" size="5" min="0.00" max="10000.00" maxlength="6"step="0.01" name="w' + weekNo + 'cost" id="w' + weekNo + 'cost3"></td><td><p id="w' + weekNo + 'people3" class="w' + weekNo + 'people">0</p></td><td><input type="checkbox" id="w' + weekNo + 'all3" onclick="selectAll('+weekNo+' , 3)">Select All</input></td></tr>';
+        tableToAdd += '<tr id="w' + weekNo + 'thurs"><td>Thursday</td><td><input type="currency" placeholder="0.00" size="5" min="0.00" max="10000.00" maxlength="6"step="0.01" name="w' + weekNo + 'cost" id="w' + weekNo + 'cost4"></td><td><p id="w' + weekNo + 'people4" class="w' + weekNo + 'people">0</p></td><td><input type="checkbox" id="w' + weekNo + 'all4" onclick="selectAll('+weekNo+' , 4)">Select All</input></td></tr>';
+        tableToAdd += '</table>';
+
+        tableToAdd += ' <br></br>';
+    }
+
+
+    document.getElementById('tblCosts').innerHTML += tableToAdd;
+
+
+    document.getElementById('tblCosts').innerHTML += '<input type="button" class="btn" value="Submit" onclick="costMeals();" />';
+}
+
+function selectAll(week, day){
+    var peoplePres = document.getElementsByClassName('w'+week+'present'+day);
+   
+    if(peoplePres.length !=0){
+        for(i=0;i<peoplePres.length;i++){
+            peoplePres[i].checked = true;
+        }
+    }
+
+    updatePeople(day, week);
+}
 
 function foodCosts() {
-    mealCosts = [];
-    for (x = 0; x < 4; x++) {
-        var day = "cost" + (x + 1);
-        var dayCost = (parseFloat(document.getElementById(day).value));
-        var costs = "people" + (x + 1);
-        var peoplePresent = (parseFloat(document.getElementById(costs).value));
+    //per week
+    for (y = 0; y < numWeeks; y++) {
+        //per day
+        for (x = 0; x < 4; x++) {
+            var day = "w" + (y + 1) + "cost" + (x + 1);
+            var dayCost = (parseFloat(document.getElementById(day).value));
+            var costs = "w" + (y + 1) + "people" + (x + 1);
+            var peoplePresent = (parseFloat(document.getElementById(costs).innerHTML));
 
-        mealCosts.push(dayCost / peoplePresent);
+            mealCosts.push(dayCost / peoplePresent);
+        }
     }
+
+
 }
 
-function checkValues() {
+function checkValues(week) {
 
     for (y = 0; y < 4; y++) {
-        var sub = "cost" + (y + 1);
+        var sub = "w" + week + "cost" + (y + 1);
         if (isNaN(document.getElementById(sub).value)) {
             return false;
 
@@ -79,11 +151,11 @@ function checkValues() {
     return true;
 }
 
-function isOneChecked() {
+function isOneChecked(week) {
     var chefsSelected = false;
     for (y = 0; y < 4; y++) {
         chefsSelected = false;
-        var chx = document.getElementsByName(('chef' + (y + 1)));
+        var chx = document.getElementsByName(('w' + week + 'chef' + (y + 1)));
         for (var i = 0; i < chx.length; i++) {
             if (chx[i].type == 'radio' && chx[i].checked) {
                 chefsSelected = true;
@@ -95,59 +167,104 @@ function isOneChecked() {
 }
 
 function costMeals() {
-    var allCostNums = checkValues();
-    var chefsCook = isOneChecked();
+    var allCostNums = true;
+    for (i = 0; i < numWeeks; i++) {
+        if (checkValues((i + 1)) == false) {
+            allCostNums = false;
+            break;
+        }
+    }
+
+    var chefsCook = true;
+    for (i = 0; i < numWeeks; i++) {
+        if (isOneChecked((i + 1)) == false) {
+            chefsCook = false;
+            break;
+        }
+    }
 
     if (allCostNums && chefsCook) {
+        usersCosts = [];
+        mealCosts = [];
+        usersMeals = [];
         foodCosts();
         calcPerPerson();
         generateTableOutput();
-    } else {
+    } else if(!allCostNums){
         alert("Please check that all costs are numbers");
+    } else if(!chefsCook){
+        alert("Please check to ensure a chef is selected for each day");
+    } else{
+        alert("Hm. There seems to be another issues, refresh the page and start again");
     }
 }
 
 function calcPerPerson() {
-    usersCosts = [];
-    var x = document.getElementById("costs").rows[0].cells.length;
-    document.getElementById('here').innerHTML = "";
 
     for (y = 0; y < numPeople; y++) {
         var userSpent = 0;
         var userMealCosts = 0;
-        for (x = 0; x < 4; x++) {
-            var userChef = false;
-            var userPresent = false;
-            if (document.getElementById(('chef' + (x + 1) + namePeoples[y])).checked) {
-                userChef = true;
+
+        //per week
+        var z = 1;
+        var totalnum = parseInt(numWeeks);
+        var a = totalnum + 1;
+        var counter = 1;
+        while (z < a) {
+            //per day
+            for (x = 0; x < 4; x++) {
+                var userChef = false;
+                var userPresent = false;
+                if (document.getElementById(('w' + z + 'chef' + (x + 1) + namePeoples[y])).checked) {
+                    userChef = true;
+                }
+                if (userChef == true) {
+                    userSpent += parseFloat(document.getElementById(('w' + z + 'cost' + (x + 1))).value);
+                }
+                if (document.getElementById(('w' + z + 'present' + (x + 1) + namePeoples[y])).checked) {
+                    userPresent = true;
+                }
+                //correct meal costs for that week
+                if (userPresent == true) {
+                    userMealCosts += mealCosts[(((z - 1) * 4) + x)];
+                }
+                counter++;
             }
-            if (userChef == true) {
-                userSpent += parseFloat(document.getElementById(('cost' + (x + 1))).value);
-            }
-            if (document.getElementById(('present' + (x + 1) + namePeoples[y])).checked) {
-                userPresent = true;
-            }
-            if (userPresent == true) {
-                userMealCosts += mealCosts[x];
-            }
+            z++;
         }
+
         usersCosts.push(userSpent);
         usersMeals.push(userMealCosts);
     }
 }
 
 function createCol(name, num) {
-    var row = document.getElementById("titles");
-    var x = row.insertCell(3);
-    x.innerHTML = name;
-    var days = ["mon", "tues", "wed", "thurs"];
-
-    for (i = 0; i < 4; i++) {
-        row = document.getElementById(days[i]);
+    var y = 0;
+    while (y < numWeeks) {
+        //gets name of row w1titles - Week 1 Titles of table
+        var rowName = "w" + (y + 1) + "titles";
+        var row = document.getElementById(rowName);
+        //inserts new column at end of table (adds empty column)
         var x = row.insertCell(3);
-        x.innerHTML += '<br><input type="radio" name="chef' + (i + 1) + '" value="chef' + (i + 1) + '" onclick="selectPres(' + num + ',' + (i + 1) + ');" id="chef' + (i + 1) + name + '"/>Chef?';
-        x.innerHTML += '<br><input type="checkbox" name="present' + (i + 1) + '" value="present' + (i + 1) + '" onclick="updatePeople(' + (i + 1) + ');" id="present' + (i + 1) + name + '"/>Present?';
+
+        //adds name of person
+        x.innerHTML = '<b style="color:black;">' + name + '</b>';
+
+        //gets row titles - to use
+        var days = [("w" + (y + 1) + "mon"), ("w" + (y + 1) + "tues"), ("w" + (y + 1) + "wed"), ("w" + (y + 1) + "thurs")];
+
+        for (i = 0; i < 4; i++) {
+            //gets row ID
+            row = document.getElementById(days[i]);
+            //inserts cell in that row
+            var x = row.insertCell(3);
+            //inserts chef and or present into cell as options
+            x.innerHTML += '<br><input type="radio" name="w' + (y + 1) + 'chef' + (i + 1) + '" value="w' + (y + 1) + 'chef' + (i + 1) + '" onclick="selectPres(' + num + ',' + (i + 1) + ', ' + (y + 1) + ');" id="w' + (y + 1) + 'chef' + (i + 1) + name + '"/>Chef?';
+            x.innerHTML += '<br><input type="checkbox" name="w' + (y + 1) + 'present' + (i + 1) + '" value="w' + (y + 1) + 'present' + (i + 1) + '" onclick="updatePeople(' + (i + 1) + ', ' + (y + 1) + ');" id="w' + (y + 1) + 'present' + (i + 1) + name + '" class="w' + (y + 1) + 'present' + (i + 1)+'"/>Present?';
+        }
+        y++;
     }
+
 }
 
 function generateTableOutput() {
@@ -157,6 +274,7 @@ function generateTableOutput() {
             document.getElementById("outputTable").deleteRow(1);
         }
     }
+
 
     document.getElementById('tblOutput').style.display = "block";
     var table = document.getElementById("outputTable");
@@ -168,10 +286,10 @@ function generateTableOutput() {
         cell1.innerHTML = namePeoples[y];
 
         cell1 = row.insertCell(1);
-        cell1.innerHTML = usersCosts[y];
+        cell1.innerHTML = '£' + ((usersCosts[y]).toFixed(2));
 
         cell1 = row.insertCell(2);
-        cell1.innerHTML = usersMeals[y];
+        cell1.innerHTML = '£' + ((usersMeals[y]).toFixed(2));
 
         cell1 = row.insertCell(3);
         var cell2 = row.insertCell(4);
@@ -181,24 +299,27 @@ function generateTableOutput() {
         var usersMoney = usersCosts[y] - usersMeals[y];
 
         if (usersMoney < 0) {
-            cell1.innerHTML = Math.abs(usersMoney);
+            cell1.innerHTML = '£' + ((Math.abs(usersMoney)).toFixed(2));
             cell2.innerHTML = '<span class="owes"> OWES</span>';
         } else if (usersMoney > 0) {
-            cell1.innerHTML = Math.abs(usersMoney);
+            cell1.innerHTML = '£' + ((Math.abs(usersMoney)).toFixed(2));
             cell2.innerHTML = '<span class="owed"> OWED</span>';
+        } else if (usersMoney == 0) {
+            cell1.innerHTML = '£' + 0;
         }
     }
 }
 
-function selectPres(num, day) {
-    var identify = "present" + day + namePeoples[num];
+function selectPres(num, day, week) {
+    var identify = "w" + week + "present" + day + namePeoples[num];
     document.getElementById(identify).checked = true;
-    updatePeople(day);
+    updatePeople(day, week);
 }
 
-function updatePeople(presDay) {
-    var identify = 'present' + presDay;
-    var identify2 = 'people' + presDay;
+function updatePeople(presDay, week) {
+    var identify = 'w' + week + 'present' + presDay;
+    var identify2 = 'w' + week + 'people' + presDay; 
+
     var chx = document.getElementsByName(identify);
 
     var numTicked = 0;
@@ -207,5 +328,5 @@ function updatePeople(presDay) {
             numTicked++;
         }
     }
-    document.getElementById(identify2).value = numTicked;
+    document.getElementById(identify2).innerHTML = numTicked;
 }
